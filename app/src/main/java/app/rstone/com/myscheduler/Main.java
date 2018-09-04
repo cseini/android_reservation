a637193d3353392786c31a106223c75b39c655e4
cseini <cseini@nate.com>
2018-09-04 18:30:59 +0900

    reservation test

---
+++ app/src/main/java/app/rstone/com/myscheduler/Main.java
@@ -0,0 +1,63 @@
+package app.rstone.com.myscheduler;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main extends AppCompatActivity {
+    String date;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        Context ctx = Main.this;
+        TextView today = findViewById(R.id.today);
+        today.setText(new SimpleDateFormat("yyyy년 MM월 dd일 hh시 mm분 ss초").format(new Date()));
+        CalendarView calender = findViewById(R.id.calender);
+        TimePicker time = findViewById(R.id.time);
+        TextView year = findViewById(R.id.year);
+        TextView month = findViewById(R.id.month);
+        TextView day = findViewById(R.id.day);
+        TextView hour = findViewById(R.id.hour);
+        TextView minute = findViewById(R.id.minute);
+        time.setVisibility(View.INVISIBLE);
+        findViewById(R.id.rdoCalendar).setOnClickListener(
+            (View v)->{
+                calender.setVisibility(View.VISIBLE);
+                time.setVisibility(View.INVISIBLE);
+            }
+        );
+        findViewById(R.id.rdoTime).setOnClickListener(
+            (View v)->{
+                calender.setVisibility(View.INVISIBLE);
+                time.setVisibility(View.VISIBLE);
+            }
+        );
+        findViewById(R.id.btnEnd).setOnClickListener(
+            (View v)->{
+                year.setText(date.substring(0,date.indexOf("/")));
+                month.setText(date.substring(date.indexOf("/")+1,date.lastIndexOf("/")));
+                day.setText(date.substring(date.lastIndexOf("/")+1));
+                hour.setText(String.valueOf(time.getHour()));
+                minute.setText(String.valueOf(time.getMinute()));
+                }
+        );
+        calender.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                date = year+"/"+month+"/"+dayOfMonth;
+            }
+        });
+    }
+}
